282d01017a49b1587dc87d355b10806f2a31a041
VanTej <vanessatejedor@gmail.com>
2022-08-04 17:57:18 +0200

    RB6 quest

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import Contact from "./Contact";
 export const App = (
   <div>
     <h1>Mes contacts</h1>
-    <Contact name="Eva Hanchier" phone="📞 03.49.79.52.46" email="evah@gmail.com"/>
-    <Contact name="Yvon Hanchier" phone="📞 03.49.79.52.46" email="yvonh@gmail.com"/>
-    <Contact name="Jean Chie" phone="📞 03.49.79.52.46" email="jeanc@gmail.com"/>
-    <Contact name="Maman" phone="📞 03.49.79.52.46" email="mamere@gmail.com"/>
-    <Contact name="Maison" phone="📞 03.49.79.52.46" email="maison@gmail.com"/>
+    <Contact name="Eva Hanchier" phone="📞 03.49.79.52.46" email="evah@gmail.com" isDeletable={true} />
+    <Contact name="Yvon Hanchier" phone="📞 03.49.79.52.46" email="yvonh@gmail.com" isDeletable={true}/>
+    <Contact name="Jean Chie" phone="📞 03.49.79.52.46" email="jeanc@gmail.com" isDeletable={true}/>
+    <Contact name="Maman" phone="📞 03.49.79.52.46" email="mamere@gmail.com" isDeletable={false}/>
+    <Contact name="Maison" phone="📞 03.49.79.52.46" email="maison@gmail.com" isDeletable={false}/>
   </div>
 );
 
