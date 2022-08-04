9ad7d8485f48a8df9aed99d772869c7e256ab00d
VanTej <vanessatejedor@gmail.com>
2022-08-04 17:14:26 +0200

    RB5 quest

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import Contact from './Contact';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import Contact from "./Contact";
 
 export const App = (
   <div>
-    <Contact />
+    <h1>Mes contacts</h1>
+    <Contact name="Eva Hanchier" phone="📞 03.49.79.52.46" email="evah@gmail.com"/>
+    <Contact name="Yvon Hanchier" phone="📞 03.49.79.52.46" email="yvonh@gmail.com"/>
+    <Contact name="Jean Chie" phone="📞 03.49.79.52.46" email="jeanc@gmail.com"/>
+    <Contact name="Maman" phone="📞 03.49.79.52.46" email="mamere@gmail.com"/>
+    <Contact name="Maison" phone="📞 03.49.79.52.46" email="maison@gmail.com"/>
   </div>
 );
 
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById("root"));
